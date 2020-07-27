a3a9b36993aa622406c22e6f1cec4453ee65325c
NIZAMIL PUTRA <nizamil.putra@gmail.com>
2020-07-27 17:58:29 +0700

    change swiper css

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -5,8 +5,7 @@ import { motion } from "framer-motion"
 import { useInView } from 'react-intersection-observer'
 
 import Gradient from "../images/Testimonial/TestimonialGradient.svg"
-import "swiper/swiper.scss"
-import "swiper/components/pagination/pagination.scss"
+import "swiper/swiper-bundle.min.css"
 import "./testimonial.scss"
 
 SwiperCore.use([Pagination, A11y]);
@@ -55,7 +54,7 @@ const Testimonial = () => {
                                     I never found app like this. It keeps me motivated to reach my goal. I have some new friends who have similar passion like me. I feel like I starting new life. 
                                 </h3>
                                 <div className="testimonial__author">
-                                    <img src="http://source.unsplash.com/56x56/?face,man" alt="Bond"/>
+                                    <img src="https://source.unsplash.com/56x56/?face,man" alt="Bond"/>
                                     <div className="testimonial__author__detail">
                                         <h4>John Bond</h4>
                                         <p>Bussiness Advisor </p>
@@ -68,7 +67,7 @@ const Testimonial = () => {
                                     Adding a customer quote only increases your chances of converting your site’s traffic. If you can, add a high-quality image of the customer providing the quote.
                                 </h3>
                                 <div className="testimonial__author">
-                                    <img src="http://source.unsplash.com/56x56/?face,girl" alt="Elena"/>
+                                    <img src="https://source.unsplash.com/56x56/?face,girl" alt="Elena"/>
                                     <div className="testimonial__author__detail">
                                         <h4>Elena Stan</h4>
                                         <p>Graphic Designer</p>
@@ -81,7 +80,7 @@ const Testimonial = () => {
                                     Just great! Thank you for making this app.
                                 </h3>
                                 <div className="testimonial__author">
-                                    <img src="http://source.unsplash.com/56x56/?face,man,black" alt="Bobby"/>
+                                    <img src="https://source.unsplash.com/56x56/?face,man,black" alt="Bobby"/>
                                     <div className="testimonial__author__detail">
                                         <h4>Bobby Dogh</h4>
                                         <p>Lawyer</p>
